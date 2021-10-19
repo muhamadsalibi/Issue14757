3b1366ed1b2bec7e89a1d67c6f269c54d96ac2e8
m.salibi <muhamadsalibi@gmail.com>
2021-10-19 10:06:15 +0200

    Add project files.

---
+++ TestApp/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace TestApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<int> Values { get; set; } = new ObservableCollection<int>();
+
+        public static readonly BindableProperty SomeTextProperty = BindableProperty.Create(nameof(SomeText), typeof(string), typeof(MainPage), default(string));
+        public string SomeText
+        {
+            get { return (string)GetValue(SomeTextProperty); }
+            set { SetValue(SomeTextProperty, value); }
+        }
+
+        public MainPage()
+        {
+            SomeText = "Default";
+
+            for (int i = 0; i <= 1000; i++)
+                Values.Add(i);
+
+            BindingContext = this;
+
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NewPage(this));
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            GC.Collect();
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            SomeText = SomeText == "Default" ? "Some Value" : "Default";
+        }
+    }
+}
---
+++ TestApp/NewPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewPage : ContentPage
+    {
+        public MainPage MyMainPage { get; set; }
+
+        public NewPage(MainPage mainpage)
+        {
+            BindingContext = MyMainPage = mainpage;
+           
+            InitializeComponent();
+        }
+    }
+}
